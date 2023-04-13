54d40ff2f75cc599b3d4e981fb55911ede2f2edd
shahidmonowarr <shahidmonowar21@gmail.com>
2023-04-13 14:33:02 +0600

    Exploring file system module

--- first-node-server/index.js
+++ first-node-server/index.js
@@ -1,76 +1,38 @@
-// local modules
-const {add, name} = require('./other');
-
-const res = add(1, 2);
-// console.log(res);
-
-// core modules
-const http = require('http');
+// Exploring File System Module
+const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    // res.end('Hello Monu');
-    // console.log(req.url);
-    if(req.url == '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<p>Home Page</p>');
-        res.end();
-    }
+  if ((req.url = "/")) {
+    // asynchronous way of reading file
+    // fs.readFile("data.txt", (err, data) => {
+    //   if (err) {
+    //     res.write("Failed to read data");
+    //     res.end();
+    //   } else {
+    //     res.write(data);
+    //     res.end();
+    //   }
+    // });
 
-    else if(req.url == '/contact') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<p>Contact Page</p>');
-        res.end();
-    }
+    // synchronous way of reading file
+    // const data = fs.readFileSync("data.txt");
+    // res.write(data);
+    // res.end();
 
-    else if(req.url == '/products') {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.write(JSON.stringify({name: 'Monowar', age: 22}));
-        res.end();
-    }
-
-    else if(req.url == '/about') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<p>About Page</p>');
-        res.end();
-    }
-
-    else {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.write('<p>404 Page Not Found</p>');
-        res.end();
-    }
+    // asynchronous way of writing file
+    fs.writeFile("newData.txt", "Hello Node JS ....", (err) => {
+        if(err){
+            res.write("Failed to write data");
+            res.end();
+        }
+        else{
+            res.write("Data written successfully");
+            res.end();
+        }
+    });
+  }
 });
 
-const PORT = 5000;
-
-server.listen(PORT);
-console.log(`Server is running on port ${PORT}`);
-
-
-// there are 3 types of modules in nodejs
-// 1. core modules
-// 2. local modules
-// 3. third party modules
-
-// core modules:
-// core modules are modules that are built into nodejs and are available to use without installing them.
-// to use a core module, you just require it by name and use it.
-// example:
-//const fs = require('fs');
-// fs is a core module that allows you to work with the file system on your computer.
-// you can use it to create, read, update, and delete files and folders.
-
-// local modules:
-// local modules are modules that you create yourself.
-// to use a local module, you just require it by name and use it.
-// example:
-//const myModule = require('./myModule');
-// myModule is a local module that is located in the same folder as the file that requires it.
-
-// third party modules:
-// third party modules are modules that are created by other developers and are available to use on npm.
-// to use a third party module, you just require it by name and use it.
-// example:
-//const _ = require('lodash');
-// lodash is a third party module that is available to use on npm.
-// you can use it to perform a variety of tasks such as sorting arrays, removing duplicates, and more.
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
